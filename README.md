aa1148f38a781db5be247d90a2fc94cf2aa28b1f
cweijan <cweijan@163.com>
2020-11-24 18:36:30 +0800

    quick commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# Pdman Web
+
+## 安装
+
+`#`表示执行命令
+```shell script
+# git clone https://github.com/cweijan/pdman-web.git
+
+# cd pdman_web
+
+# yarn install
+```
+
+## 运行
+
+```
+# yarn start
+```
+打开 [http://localhost:3000](http://localhost:3000)
+
+浏览器中可以看到欢迎页，就说明成功启动了。
---
+++ src/components/code/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import './style/index.less';
+/* eslint-disable */
+export default class Code extends React.Component{
+  componentDidMount(){
+    this.dom = ReactDom.findDOMNode(this.instance);
+  };
+  componentDidUpdate(){
+  }
+  shouldComponentUpdate(nextProps){
+    return (nextProps.data !== this.props.data) || this._checkStyle(nextProps);
+  };
+  _checkStyle = (nextProps) => {
+    const nextStyle = nextProps.style || {};
+    const thisStyle = this.props.style || {};
+    return (nextStyle.height !== thisStyle.height) || (nextStyle.width !== thisStyle.width)
+  };
+  render() {
+    const { data, style, prefix = 'pdman' } = this.props;
+    return (<div style={style} className={`${prefix}-code`}>
+      <pre ref={instance => this.instance = instance} style={style}>
+        {data}
+      </pre>
+    </div>);
+  }
+}
