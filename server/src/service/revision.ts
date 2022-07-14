91c865d510e48b9a9ac0ae6530d5844802928821
cweijan <cweijan@163.com>
2022-07-14 15:13:29 +0800

    移除sqlite

--- server/src/service/revision.ts
+++ server/src/service/revision.ts
@@ -1,5 +1,3 @@
-import sqlite3 from 'sqlite3'
-import { open } from 'sqlite'
 import { Application } from 'express';
 import { pressError } from './uti';
 import { NewProjectVersion } from '../request/requestDTO';
@@ -13,40 +11,40 @@ const dataDDL = `CREATE TABLE revision_data(
 );`
 
 function openDb() {
-  return open({
-    filename: __dirname + '/revision_data.db',
-    driver: sqlite3.Database
-  })
+  // return open({
+  //   filename: __dirname + '/revision_data.db',
+  //   driver: sqlite3.Database
+  // })
 }
 
 async function initRevision() {
-  const db = await openDb();
-  const revisionData = await db.get(`SELECT name, type FROM sqlite_master WHERE type="table" AND name='revision_data';`);
-  if (revisionData) return;
-  db.exec(dataDDL)
+  // const db = await openDb();
+  // const revisionData = await db.get(`SELECT name, type FROM sqlite_master WHERE type="table" AND name='revision_data';`);
+  // if (revisionData) return;
+  // db.exec(dataDDL)
 }
 
 export async function bindRevisionApi(app: Application) {
 
-  initRevision()
+  // initRevision()
 
-  app.get('/api/project/version', async (req, res, next) => {
-    pressError(res, async () => {
-      const projectId = req.query.projectId;
-      const db = await openDb();
-      const versionData=await db.all(`select * from revision_data where project_id=${projectId}`)
-      res.json(versionData);
-    })
-  });
+  // app.get('/api/project/version', async (req, res, next) => {
+  //   pressError(res, async () => {
+  //     const projectId = req.query.projectId;
+  //     const db = await openDb();
+  //     const versionData=await db.all(`select * from revision_data where project_id=${projectId}`)
+  //     res.json(versionData);
+  //   })
+  // });
 
-  app.post('/api/project/version/new', async function (req, res, next) {
-    pressError(res, async () => {
-      const option: NewProjectVersion = req.body;
-      const db = await openDb();
+  // app.post('/api/project/version/new', async function (req, res, next) {
+  //   pressError(res, async () => {
+  //     const option: NewProjectVersion = req.body;
+  //     const db = await openDb();
 
-      db.run(`insert into revision_data(project_id,version,remark,content) values ('${option.projectId}','${option.version}','${option.remark}','${option.content}')`)
+  //     db.run(`insert into revision_data(project_id,version,remark,content) values ('${option.projectId}','${option.version}','${option.remark}','${option.content}')`)
 
-    })
-  });
+  //   })
+  // });
 
 }
